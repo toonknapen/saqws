6c7238d7443ed00d10236a50052b722217133c4f
Toon Knapen <toon.knapen@gmail.com>
2019-11-19 21:08:41 +0100

    updated ssl verification

--- example/simple/broker.py
+++ example/simple/broker.py
@@ -4,13 +4,15 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-host = 'localhost'
-port = 9876
+# host = 'localhost'
+# port = 9876
+host = 'sessionawarequeue1.appspot.com'
+port = None
 
 async def main():
     queue = asyncio.Queue()
     sub_client = SAQSubClient(data_buffer=queue, formatter=lambda x: x)
-    sub_client.connect(host=host, port=port, path='/saqws')
+    sub_client.connect(scheme='https', host=host, port=port, path='/saqws')
 
     while True:
         msg = await queue.get()
--- saqws/saq_sub_client.py
+++ saqws/saq_sub_client.py
@@ -16,11 +16,11 @@ class SAQSubClient:
         self._data_buffer = data_buffer
         self._formatter = formatter
 
-    def connect(self, host, port, path, ssl_context=None):
+    def connect(self, scheme, host, port, path):
         assert path[0] == '/'
 
         loop = asyncio.get_event_loop()
-        self._task = loop.create_task(self._start(host=host, port=port, path=path, ssl_context=ssl_context))
+        self._task = loop.create_task(self._start(scheme=scheme, host=host, port=port, path=path))
 
     async def wait(self):
         await self._task
@@ -28,13 +28,12 @@ class SAQSubClient:
     def done(self):
         return self._task.done()
 
-    async def _start(self, host: str, port: int, path: str, ssl_context=None):
-        scheme = 'http' if ssl_context is None else 'https'
-        url = f"{scheme}://{host}:{port}{path}"
+    async def _start(self, scheme: str, host: str, port: int, path: str):
+        url = f"{scheme}://{host}{path}" if port is None else f"{scheme}://{host}:{port}{path}"
         try:
             async with aiohttp.ClientSession() as session:
                 logger.debug('Sub started new session')
-                async with session.ws_connect(url=url, ssl=ssl_context) as ws:
+                async with session.ws_connect(url=url) as ws:
                     logger.debug('Sub connected')
                     async for msg in ws:
                         logger.debug(f'Sub received {msg}')
