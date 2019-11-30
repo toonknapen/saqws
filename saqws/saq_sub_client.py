1602c0e403318128ac3c6c1b7ba69a362e57b4b6
Toon Knapen <toon.knapen@gmail.com>
2019-11-30 19:36:42 +0100

    subcriber trying to reconnect if server gone

--- saqws/saq_sub_client.py
+++ saqws/saq_sub_client.py
@@ -1,5 +1,6 @@
 import logging
 import aiohttp
+import aiohttp.client_exceptions
 import asyncio
 import typing
 
@@ -15,6 +16,8 @@ class SAQSubClient:
     def __init__(self, data_buffer: asyncio.Queue, formatter: typing.Callable[[str], dict]):
         self._data_buffer = data_buffer
         self._formatter = formatter
+        self._try_to_connect = True
+        self._reconnect_interval = 5
 
     def connect(self, scheme, host, port, path):
         assert path[0] == '/'
@@ -30,19 +33,24 @@ class SAQSubClient:
 
     async def _start(self, scheme: str, host: str, port: int, path: str):
         url = f"{scheme}://{host}{path}" if port is None else f"{scheme}://{host}:{port}{path}"
-        try:
-            async with aiohttp.ClientSession() as session:
-                logger.debug('Sub started new session')
-                async with session.ws_connect(url=url) as ws:
-                    logger.debug('Sub connected')
-                    async for msg in ws:
-                        logger.debug(f'Sub received {msg}')
-                        if msg.type == aiohttp.WSMsgType.TEXT:
-                            formatted_msg = self._formatter(msg.data)
-                            self._data_buffer.put_nowait(formatted_msg)
-                        else:
-                            logger.info(f'inter-websocket received:{msg}')
-            logger.info(f"ws closed")
+        while self._try_to_connect:
+            try:
+                async with aiohttp.ClientSession() as session:
+                    logger.debug('Sub started new session')
+                    async with session.ws_connect(url=url) as ws:
+                        logger.debug('Sub connected')
+                        async for msg in ws:
+                            logger.debug(f'Sub received {msg}')
+                            if msg.type == aiohttp.WSMsgType.TEXT:
+                                formatted_msg = self._formatter(msg.data)
+                                self._data_buffer.put_nowait(formatted_msg)
+                            else:
+                                logger.info(f'inter-websocket received:{msg}')
+                logger.info(f"ws closed")
 
-        except:
-            logger.exception('QSub listener encountered an exception')
+            except aiohttp.client_exceptions.ClientConnectionError:
+                logger.error(f"Failed to connect to saqws server at {url}")
+            except:
+                logger.exception('QSub listener encountered an exception')
+            logger.warning(f"Retrying to connect to server in {self._reconnect_interval}s.")
+            await asyncio.sleep(self._reconnect_interval)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saqws",
-    version="0.0.2",
+    version="0.0.3",
     author="Toon Knapen",
     author_email="find@google",
     description="Session Aware Queue on WebSocket",
