813c4d787e8bd7e839e191d0f6500cecdb35403c
Toon Knapen <toon.knapen@gmail.com>
2019-12-04 17:36:42 +0100

    improved debug logging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saqws",
-    version="0.0.8",
+    version="0.0.9",
     author="Toon Knapen",
     author_email="find@google",
     description="Session Aware Queue on WebSocket",
